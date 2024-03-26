f148f4d2b08bf660c75712c3867b2eafaa1edd9b
klaus14k <klauskuhlmann14@gmail.com>
2024-03-26 14:56:20 -0300

    feat: animations, other corrections

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,28 @@
 function handleModal() {
-    const button = document.querySelector('.join_button')
-    const modalController = document.querySelector('.modal__controller')
+    const buttons = document.querySelectorAll(".join_button")
+    const modalController = document.querySelector(".modal__controller")
 
-    button.addEventListener('click', () => {
+    buttons.forEach((button) => button.addEventListener("click", () => {
         modalController.showModal()
-    })
+    }))
     function closeModal() {
-        const button = document.querySelector('.modal__close')
-        const modalController = document.querySelector('.modal__controller')
-        
-        button.addEventListener('click', () => {
+        const button = document.querySelector(".modal__close")
+        const modalController = document.querySelector(".modal__controller")
+
+        button.addEventListener("click", () => {
             modalController.close()
         })
     }
     closeModal()
 }
 handleModal()
+
+function scrollTo() {
+    const section = document.getElementById("second_section")
+    const button = document.getElementById("button_aboutUs")
+
+    button.addEventListener("click", () => {
+        section.scrollIntoView()
+    })
+}
+scrollTo()
